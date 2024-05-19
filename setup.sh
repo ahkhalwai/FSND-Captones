a12a11109ffbbf191d60d25c51da38c1cf81d920
Halwai Aftab Hasan <48179028+ahkhalwai@users.noreply.github.com>
2024-05-20 00:40:27 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-export DATABASE_URL="postgresql://postgres:8089@localhost:5432/postgres"
+export DATABASE_URL="postgresql://postgres:8089@localhost:5432/ca"
 export EXCITED="true"
 echo "setup.sh script executed successfully!"
