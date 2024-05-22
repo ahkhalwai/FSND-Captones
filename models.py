a14e0f34ad19595fa43cbb794b315465d8bb8205
Halwai Aftab Hasan <48179028+ahkhalwai@users.noreply.github.com>
2024-05-23 01:35:12 +0530

    Update models.py

--- models.py
+++ models.py
@@ -20,6 +20,7 @@ def setup_db(app, database_path=database_path):
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.app = app
     db.init_app(app)
+    db.create_all()
 
 def db_drop_and_create_all(app):
     #db.drop_all()
