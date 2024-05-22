e0598bc3219006b63c73ee65614df0a470d0edf3
Halwai Aftab Hasan <48179028+ahkhalwai@users.noreply.github.com>
2024-05-22 19:41:42 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+export AUTH0_DOMAIN = 'dev-aiehfurehuh6sbmf.us.auth0.com'
+export ALGORITHMS = ['RS256']
+export API_AUDIENCE = '127.0.0.1:5000'
 export DATABASE_URL="postgresql://postgres:8089@localhost:5432/ca"
 export EXCITED="true"
 echo "setup.sh script executed successfully!"
