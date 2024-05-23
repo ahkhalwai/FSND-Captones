3b00539ca54cd93bb5dcc738f891d03d8900ef11
ahkhalwai55 <ahkhalwai55@gmail.com>
2024-05-23 21:36:43 +0530

    bin updates

--- env/bin/activate.fish
+++ env/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/musef/Udacity/FSND/render-deployment-example/env"
+set -gx VIRTUAL_ENV "/Users/musef/Udacity/FSND/FSND-Captones/env"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
